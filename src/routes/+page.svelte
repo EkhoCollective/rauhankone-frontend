bd8727b4d2a831ec9bf50b5bd8ccb48810351aea
calvinguillot <calvinguillot@gmail.com>
2025-02-24 12:01:17 +0000

    daisyUI updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
 <script>
-    import { Canvas } from '@threlte/core'
-    import Scene from '../lib/Scene/Scene.svelte'
-  </script>
-  
-  <Canvas>
-    <Scene />
-  </Canvas>
+	import { Canvas } from '@threlte/core';
+	import Scene from '../lib/Scene/Scene.svelte';
+</script>
+
+<svelte:head>
+	<title>Rauhankoneen Kerroksia</title>
+</svelte:head>
+
+<div class="homepage-title-card">
+	<h1>Layers in the Peace Machine</h1>
+	<p>
+		... is part of the dynamic discourse about actions, choices, mutual interactions and power,
+		which all greatly affect the processes of peace.
+	</p>
+
+	<button class="btn btn-primary">Button</button>
+</div>
+
+<div class="homepage-scene">
+	<Canvas>
+		<Scene />
+	</Canvas>
+</div>
+
+<style>
+	.homepage-scene {
+		background-color: black;
+		width: 100vw;
+		height: 100vh;
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: 1; /* Ensure the canvas is behind other elements */
+	}
+
+	.homepage-title-card {
+		z-index: 9999; /* Ensure this is on top of everything */
+		position: relative; /* Ensure it is positioned above the canvas */
+	}
+</style>
