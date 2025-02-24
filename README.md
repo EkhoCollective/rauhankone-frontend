5d1eb6b5fa4741bde2f6f4403120e25878444917
calvinguillot <calvinguillot@gmail.com>
2025-02-24 10:53:17 +0000

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rauhankone-frontend
+
+This is the main repository for the frontend of the Rauhankone project. The project is a web application that allows users to create and share peace agreements. The project is built with SvelteKit.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<script>
+    import { Canvas } from '@threlte/core'
+    import Scene from '../lib/Scene/Scene.svelte'
+  </script>
+  
+  <Canvas>
+    <Scene />
+  </Canvas>
