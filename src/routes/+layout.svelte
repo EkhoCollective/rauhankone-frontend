616727bcb759ae181475f2024c1a7b4eeba9c8a2
calvinguillot <calvinguillot@gmail.com>
2025-06-09 14:25:49 +0300

    Test remove LayoutLoad type

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,14 +4,14 @@
 	import '$lib/i18n';
 	import { onMount } from 'svelte';
 	import { locale, waitLocale } from 'svelte-i18n';
-	import type { LayoutLoad } from './$types';
+	// import type { LayoutLoad } from './$types';
 
-	export const load: LayoutLoad = async () => {
-		if (browser) {
-			locale.set(window.navigator.language);
-		}
-		await waitLocale();
-	};
+	// export const load: LayoutLoad = async () => {
+	// 	if (browser) {
+	// 		locale.set(window.navigator.language);
+	// 	}
+	// 	await waitLocale();
+	// };
 
 	onMount(() => {
 		if (browser) {
