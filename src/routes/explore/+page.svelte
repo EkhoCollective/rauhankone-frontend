209c22e1edeff8d10fdb004b5d7db37393f81402
calvinguillot <calvinguillot@gmail.com>
2025-08-13 12:31:44 +0300

    Fixed modal contrast

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -193,10 +193,13 @@
 
 	.modal-story-container {
 		backdrop-filter: blur(10px);
+		background-color: rgba(0, 0, 0, 0.4);
 		position: absolute;
 		z-index: 100;
 		top: 0;
 		left: 0;
+		width: 100vw;
+		height: 100vh;
 	}
 
 	/* .audio-icon-container {
