5588a6ab17f90c1c24f0a53ba07ce90b6355488b
calvinguillot <calvinguillot@gmail.com>
2025-08-19 12:01:00 -0500

    Updates

---
+++ _app/immutable/chunks/Z_d5nBXf.js
@@ -0,0 +1,39 @@
+import"./BniEmjqM.js";import{$ as h,a0 as u,V as l,_ as k,O as g,Q as b,R as f,X as d,W as V}from"./BdwEzn_I.js";import{i as q}from"./DagnvrFS.js";import{l as m,s as p,p as y}from"./ZPzR1z2S.js";import"./bwUnXfoF.js";import{I as _,s as $}from"./BvlJ5lqz.js";function N(r,e){const a=m(e,["children","$$slots","$$events","$$legacy"]);/**
+ * @license lucide-svelte v0.503.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */const c=[["path",{d:"M21 10.5V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h12.5"}],["path",{d:"m9 11 3 3L22 4"}]];_(r,p({name:"square-check-big"},()=>a,{get iconNode(){return c},children:(t,n)=>{var o=h(),i=u(o);$(i,e,"default",{}),l(t,o)},$$slots:{default:!0}}))}function x(r,e){const a=m(e,["children","$$slots","$$events","$$legacy"]);/**
+ * @license lucide-svelte v0.503.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */const c=[["rect",{width:"18",height:"18",x:"3",y:"3",rx:"2"}]];_(r,p({name:"square"},()=>a,{get iconNode(){return c},children:(t,n)=>{var o=h(),i=u(o);$(i,e,"default",{}),l(t,o)},$$slots:{default:!0}}))}var C=(r,e)=>e(),I=b('<div class="checkmark-container"><button class="checkmark-btn"><!></button></div>');function M(r,e){g(e,!0);let a=y(e,"checkValue",15,!1);function c(){a(!a())}var t=I(),n=f(t);n.__click=[C,c];var o=f(n);{var i=s=>{N(s,{color:"#ffffff"})},v=s=>{x(s,{color:"#ffffff"})};q(o,s=>{a()?s(i):s(v,!1)})}d(n),d(t),l(r,t),V()}k(["click"]);export{M as C};
