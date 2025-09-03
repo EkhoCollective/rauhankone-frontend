e19d7fb724de3a112b9e626187cc9fe6362ffd1d
Aavato-c <80039165+Aavato-c@users.noreply.github.com>
2025-09-03 11:11:15 +0300

    Add: Implement footer with localized logo display

--- src/lib/components/mini-components/Footer.svelte
+++ src/lib/components/mini-components/Footer.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
 	import { _ } from 'svelte-i18n';
+	import logoFin from '../media/Oulu2026_logo_white_RGB_FIN_horizontal.png'; // 640 × 240
+	import logoEng from '../media/Oulu2026_logo_white_RGB_FIN_horizontal.png'; // 640 × 259
+
+
+	const locale = localStorage.getItem('locale') || 'en';
+	// Set correct logo based on locale
+	const logo = locale === 'fi' ? logoFin : logoEng;
+
 </script>
 
 <!-- Footer -->
@@ -14,6 +22,9 @@
 		<a href={$_('main_link_ekho')} target="_blank" rel="noopener noreferrer">© Ekho Collective</a>
 	</div>
 	<div><a href={$_('main_link_gdpr')} target="_blank" rel="noopener noreferrer">{$_('main_link_gdpr_title')}</a></div>
+	<div class="logocontainer">
+		<img src={logo} alt="Oulu 2026 Logo" class="footer-logo" />
+	</div>
 </footer>
 
 <style>
@@ -22,6 +33,16 @@
 		display: flex;
 		flex-direction: column;
 		text-decoration: unset;
+	}
+
+	.logocontainer {
+		width: 200px;
+		height: 100%;
+		transform: translateX(-17px);
+	}
+	.footer-logo {
+		width: 100%;
+		height: 100%;
 	}
 
 		@media (max-width: 768px) {
