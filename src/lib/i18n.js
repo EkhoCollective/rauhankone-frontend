5fce6d14821b0583c45a31f9e0e55290724c351f
Aavato-c <80039165+Aavato-c@users.noreply.github.com>
2025-09-03 17:44:49 +0300

    Fix: Updatedefault locale handling

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -4,12 +4,12 @@ import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
 const defaultLocale = 'en';
 
 register('en', () => import('$lib/translations/en.json'));
-register('en-GB', () => import('$lib/translations/en.json'));
-register('fi-FI', () => import('$lib/translations/fi.json'));
-
+/* register('en-GB', () => import('$lib/translations/en.json'));
+register('fi-FI', () => import('$lib/translations/fi.json')); */
 register('fi', () => import('$lib/translations/fi.json'));
-register('sv', () => import('$lib/translations/sv.json'));
-register('sa', () => import('$lib/translations/sa.json'));
+
+//register('sv', () => import('$lib/translations/sv.json'));
+//register('sa', () => import('$lib/translations/sa.json'));
 
 const supportedLocales = ['en', 'fi', 'en-GB'];
 
