5f0da0251eebd17648785c75d0afe35a7eeb61af
Aavato-c <80039165+Aavato-c@users.noreply.github.com>
2025-11-10 12:26:24 +0200

    refactor consts.ts: run against prod backend

--- src/lib/utils/consts.ts
+++ src/lib/utils/consts.ts
@@ -1,5 +1,5 @@
 const restEnv = import.meta.env.VITE_REST_ENV;
-export const baseUrl = restEnv === 'development' ? 'https://api.peacemachine.eu' : 'https://prod.peacemachine.eu';
+export const baseUrl = restEnv === 'development' ? 'https://prod.peacemachine.eu' : 'https://prod.peacemachine.eu';
 console.log('API Base URL:', baseUrl);
 //export const baseUrl = 'https://prod.peacemachine.eu';
 
